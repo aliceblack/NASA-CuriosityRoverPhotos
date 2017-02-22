c0629bc94b8741fd6669b61b64bbc38d618c9fd3
aliceblack <blackfluid44@gmail.com>
2017-02-22 14:04:05 +0100

    camera full_name

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ echo "<p>year ".$year."</pp>";
 $id=$update["photos"]["0"]["id"];
 $id2=$update["photos"][0]["id"];
 //$name=$update["photos"]["0"]["camera"]["name"];
-$name=$update["photos"]["0"]["camera"]["id"]["name"];
+$name=$update["photos"]["0"]["camera"][$id]["full_name"];
 $url=$update["photos"]["0"]["img_src"];
 
 echo "<p>id = ".$id."</pp>";
